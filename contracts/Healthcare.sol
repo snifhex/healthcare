fa10bb9f844124e2325805fd8795a8008861e30a
Sachin Tripathi <sachintripathis84@gmail.com>
2019-03-10 02:08:42 +0530

    Update Healthcare.sol

--- contracts/Healthcare.sol
+++ contracts/Healthcare.sol
@@ -1,1 +1,16 @@
 Place holder for the solidity contract code.
+
+
+Dummy Contract until then
+
+
+pragma solidity ^0.5.0;
+
+contract MyContract {
+  uint id;
+  
+  function set(uint _id) public {
+      id = _id;
+  }
+
+}
